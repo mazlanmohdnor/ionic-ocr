e157dfee70e9e4f82193ea7bae34ebe39455d768
mazlanmohdnor <lan.psis@gmail.com>
2020-06-06 02:05:21 +0800

    first commit

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,18 +3,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { OCR } from '@ionic-native/ocr/ngx';
+import { Camera } from '@ionic-native/camera/ngx';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ExploreContainerComponentModule,
-    Tab1PageRoutingModule
-  ],
-  declarations: [Tab1Page]
+    imports: [
+        IonicModule,
+        CommonModule,
+        FormsModule,
+        Tab1PageRoutingModule
+    ],
+    declarations: [Tab1Page],
+    providers: [
+        OCR,
+        Camera,
+    ]
 })
-export class Tab1PageModule {}
+export class Tab1PageModule {
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { OCR, OCRResult, OCRSourceType } from '@ionic-native/ocr/ngx';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+const win: any = window;
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+    text: string;
+    image = 'assets/scan.png';
+    foundText: boolean = false;
 
-  constructor() {}
+    constructor(
+        private ocr: OCR,
+        private camera: Camera,
+    ) {
+    }
 
+    async onScan() {
+        // camera
+        const options: CameraOptions = {
+            quality: 100,
+            destinationType: this.camera.DestinationType.FILE_URI,
+            encodingType: this.camera.EncodingType.PNG,
+            mediaType: this.camera.MediaType.PICTURE,
+            saveToPhotoAlbum: false,
+            allowEdit: false,
+            sourceType: 1,
+            correctOrientation: true
+        };
+        const imageData = await this.camera.getPicture(options);
+        console.log('imageData :', imageData);
+        this.image = win.Ionic.WebView.convertFileSrc(imageData);
+        const ocrResult: OCRResult = await this.ocr.recText(OCRSourceType.NORMFILEURL, imageData);
+        console.log('ocrResult :', ocrResult);
+        this.foundText = ocrResult.foundText;
+        this.text = ocrResult && ocrResult.blocks && ocrResult.blocks.blocktext.toString();
+    }
+
+
+    reset() {
+        this.foundText = false;
+        this.image = undefined;
+        this.text = undefined;
+        this.camera.cleanup();
+    }
 }
